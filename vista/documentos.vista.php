555b6d5503338fb97addf0240bfe932739d4c1a2
Dennis Mauricio Aviles Odar <dennisaviles@MacBook-Pro-de-Dennis.local>
2017-01-28 09:12:33 -0500

    .

--- vista/documentos.vista.php
+++ vista/documentos.vista.php
@@ -85,7 +85,8 @@ require '../util/funciones/definiciones.php';
                         <div class="col-xs-2">
                             <label>Desde:&nbsp;</label>
                             <div class="input-group">
-                                <input type="date" id="txtfecha1" class="form-control input-sm" value="2016-11-02"/>
+                                <!--<input type="date" id="txtfecha1" class="form-control input-sm" value="2016-11-02"/-->
+                                <input type="date" id="txtfecha1" class="form-control input-sm" data-date-format="DD-MM-YYYY" value="2016-11-02"/>
                             </div><!-- /.input group -->
                         </div>
                         <div class="col-xs-2">
