4221fc75ac5b4b6791da5846d370a53f5cd5a6da
Dennis Mauricio Aviles Odar <dennisaviles@MacBook-Pro-de-Dennis.local>
2017-01-29 14:21:28 -0500

    .

--- controladores/documento.agregar.editar.php
+++ controladores/documento.agregar.editar.php
@@ -24,7 +24,7 @@ try {
     $objDocumento->setCodigo_tipo_documento($datosFormularioArray["cbotipodocumentomodal"]);
     
     if(move_uploaded_file($_FILES['p_foto']['tmp_name'], $target_path)){
-        $objDocumento->setFoto(basename($_FILES['p_foto']['name']));
+        $objDocumento->setFoto($target_path);
     }
     
     if ($datosFormularioArray["txttipooperacion"] == "agregar") {
