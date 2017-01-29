d74f2218131d687b2b0680dc92970b82678f10ff
Dennis Mauricio Aviles Odar <dennisaviles@MacBook-Pro-de-Dennis.local>
2017-01-28 20:10:11 -0500

    .

--- controladores/documento.agregar.editar.php
+++ controladores/documento.agregar.editar.php
@@ -11,10 +11,9 @@ $datosFormulario = $_POST["p_datosFormulario"];
 parse_str($datosFormulario, $datosFormularioArray);
 try {
     
-    $nombre_img = $_FILES['imagen']['name'];
-    $tipo_img = $_FILES['imagen']['type'];
-    $tamano = $_FILES['imagen']['size'];
-    $directorio = $_SERVER['DOCUMENT_ROOT'].'/fotos_documentos/';
+    $directorio = '../fotos_documentos/';
+    $imagen_subida_temporal = $_FILES['imagen']['tmp_name'];
+    $imagen_subida_nombre = $directorio.$_FILES['imagen']['name'];
     
     $objDocumento = new Documento();
     
@@ -23,9 +22,11 @@ try {
     $objDocumento->setMonto($datosFormularioArray["txtmonto"]);
     $objDocumento->setCodigo_usuario($datosFormularioArray["txtusuario"]);
     $objDocumento->setCodigo_tipo_documento($datosFormularioArray["cbotipodocumentomodal"]);
-    $objDocumento->setFoto($nombre_img);
     
-    move_uploaded_file($_FILES['imagen']['tmp_name'], $directorio.$nombre_img);
+    if(is_uploaded_file($imagen_subida_temporal)){
+        move_uploaded_file($imagen_subida_temporal, $imagen_subida_nombre);
+        $objDocumento->setFoto($imagen_subida_nombre);
+    }
     
     if ($datosFormularioArray["txttipooperacion"] == "agregar") {
         $resultado = $objDocumento->agregar();
